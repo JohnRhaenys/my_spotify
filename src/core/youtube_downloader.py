0e233eeedd07ad1989b70e7fc87e497ee5acccd9
JohnRhaenys <joao.estrela@sga.pucminas.br>
2021-09-12 17:06:11 -0300

    Youtube downloader finished

--- src/core/youtube_downloader.py
+++ src/core/youtube_downloader.py
@@ -5,16 +5,20 @@ from src.core.utils import utils
 
 
 def download_mp3(video_url, destination_folder=os.getcwd()):
-    yt = YouTube(video_url)
+    try:
+        yt = YouTube(video_url)
 
-    video = yt.streams.filter(only_audio=True).first()
+        video = yt.streams.filter(only_audio=True).first()
 
-    downloaded_file_path = video.download(destination_folder)
+        downloaded_file_path = video.download(destination_folder)
 
-    separator = os.path.sep
-    refactored_file_path = downloaded_file_path[:downloaded_file_path.rfind(separator)]
-    video_id = utils.get_youtube_video_id(video_url)
+        separator = os.path.sep
+        refactored_file_path = downloaded_file_path[:downloaded_file_path.rfind(separator)]
+        video_id = utils.get_youtube_video_id(video_url)
 
-    new_file_path = f'{refactored_file_path}{separator}{video_id}.mp3'
+        new_file_path = f'{refactored_file_path}{separator}{video_id}.mp3'
+        os.rename(downloaded_file_path, new_file_path)
+        return True
 
-    os.rename(downloaded_file_path, new_file_path)
+    except Exception:
+        return False
