d4abc72125d3bd6897c8a76470397a919b7679c1
JohnRhaenys <joao.estrela@sga.pucminas.br>
2021-09-28 22:44:30 -0300

    Playlist model: get songs function implemented

--- src/core/database/models/playlist_model.py
+++ src/core/database/models/playlist_model.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, ForeignKey, Table, DateTime
 from sqlalchemy.orm import relationship
+
 from src.core.database.models.base import Base
 
 association_table = Table('playlists_songs', Base.metadata,
@@ -20,3 +21,9 @@ class Playlist(Base):
     songs = relationship('Song', secondary=association_table)
     created_at = Column('created_at', DateTime, nullable=False, default=datetime.utcnow)
     updated_at = Column('updated_at', DateTime, nullable=False, default=datetime.utcnow)
+
+    def get_songs(self, engine):
+        query = f'SELECT song_id FROM playlists_songs WHERE playlist_id = {self.id};'
+        result = engine.execute(query)
+        songs = [row[0] for row in result]
+        return songs
