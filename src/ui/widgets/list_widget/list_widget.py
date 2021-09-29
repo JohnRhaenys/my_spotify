ae5ebf1e393971855cd873f2223470c5168e1d75
JohnRhaenys <joao.estrela@sga.pucminas.br>
2021-09-28 22:48:23 -0300

    List widget updated: item clicked event added

--- src/ui/widgets/list_widget/list_widget.py
+++ src/ui/widgets/list_widget/list_widget.py
@@ -1,22 +1,36 @@
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QWidget, QListWidget, QListWidgetItem, QHBoxLayout
 
+from src.ui.widgets.list_widget import item_data_manager
+from src.ui.widgets.playlist_card.playlist_card import PlaylistCard
+
 
 class ListWidget(QWidget):
-    def __init__(self, items=None, parent=None):
+    def __init__(self, main_window_reference, items=None, parent=None):
         super(ListWidget, self).__init__(parent)
+
+        self.main_window_reference = main_window_reference
+
         if items is None:
             items = []
+
         layout = QHBoxLayout()
+
         self.items = items
         self.list = QListWidget(self)
         layout.addWidget(self.list)
-        self.set_styles()
         self.setLayout(layout)
+
         self.set_items(self.items)
+        self.set_styles()
+        self.set_listeners()
 
     def set_styles(self):
         pass
+
+    def set_listeners(self):
+        self.list.itemClicked.connect(self.item_clicked)
 
     def set_thumbnail_image(self, image_path):
         self.video_thumbnail.setPixmap(QPixmap(image_path))
@@ -27,10 +41,20 @@ class ListWidget(QWidget):
             self.list.addItems(items)
         else:
             for item in items:
-                widget_item = QListWidgetItem(self.list)
-                widget_item.setSizeHint(item.sizeHint())
-                self.list.addItem(widget_item)
-                self.list.setItemWidget(widget_item, item)
+                q_list_item = QListWidgetItem(self.list)
+                q_list_item.setSizeHint(item.sizeHint())
+
+                self.list.addItem(q_list_item)
+                self.list.setItemWidget(q_list_item, item)
+
+                data = item_data_manager.construct_item_data(item)
+                q_list_item.setData(Qt.UserRole, data)
+
+    def item_clicked(self, item):
+        item_info = item_data_manager.fetch_item_data(item)
+        if item_info['classname'] == PlaylistCard.__name__:
+            playlist_id = item_info['data']['id']
+            self.main_window_reference.populate_songs_list(playlist_id=playlist_id)
 
     def clear(self):
         self.list.clear()
