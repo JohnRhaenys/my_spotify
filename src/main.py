e7c3e5cf9d6b068584bf85dc7d99d480b78def11
JohnRhaenys <joao.estrela@sga.pucminas.br>
2021-09-28 22:48:06 -0300

    main.py now creates the tables need for running the app (it doesn't do it if they're already created)

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
+import sys
 from PyQt5 import QtWidgets
-
+from src.core.database import data_manager
 from ui.main_window.main_window import MainWindow
 
 if __name__ == "__main__":
-    import sys
+    data_manager.create_tables()
     app = QtWidgets.QApplication(sys.argv)
     main_window = QtWidgets.QMainWindow()
     ui = MainWindow(main_window)
