e7320f0024962cf71c7eb8643818751bd7dcf1a0
JohnRhaenys <joao.estrela@sga.pucminas.br>
2021-09-28 22:45:05 -0300

    Song model: thumbnail path incorrect column name fixed

--- src/core/database/models/song_model.py
+++ src/core/database/models/song_model.py
@@ -8,9 +8,10 @@ class Song(Base):
     __tablename__ = 'songs'
 
     id = Column('id', Integer, primary_key=True)
+    thumbnail_path = Column('thumbnail_path', String(255), nullable=True)
     name = Column('name', String(255), nullable=False)
     duration_in_seconds = Column('duration', Integer, nullable=False)
-    thumbnail_path = Column('thumnail_path', String(255), nullable=True)
+    video_id = Column('video_id', String(255), nullable=False)
     file_path = Column('file_path', String(255), nullable=False)
     created_at = Column('created_at', DateTime, nullable=False, default=datetime.utcnow)
     updated_at = Column('updated_at', DateTime, nullable=False, default=datetime.utcnow)
